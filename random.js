c2787ba97cbfdd1c78df0b269389d6905e5dfbad
Heriberto Cortés <hcorteszu@gmail.com>
2015-06-20 01:13:07 -0300

    modulo5-p2p versión 2

--- random.js
+++ random.js
@@ -1,9 +1,9 @@
 // Math.random() devuelve número aleatorio entre 0 y 1.
 var numero = Math.random();
 
+var str = ' Mayor que 0,5';
+
 if (numero <= 0.5){
-  console.log('\n' + numero + ' MENOR que 0,5 \n');
+  str = ' Menor que 0,5';
 }
-else {
-  console.log('\n' + numero + ' Mayor que 0,5 \n');
-}
+  console.log('\n' + numero + str + '\n');
